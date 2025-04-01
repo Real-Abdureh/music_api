a8639630ba03370d5498cc4307b3cf3094ef003b
Abdulrahman Abbas <abdouliabbas75@gmail.com>
2025-04-01 12:34:55 +0100

    add read me and .env

--- music_booking/settings.py
+++ music_booking/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_zx*zg8v)2@)%y!%pl)uut=+74jxgp7a58q30h%nqd^(!c6r^z'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = DEBUG = config('DEBUG', cast=bool )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1,localhost']
 
 
 # Application definition
